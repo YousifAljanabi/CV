eab890230f77cfb1aa11b7025beb8f579564e048
Mohammed Hayder <mohammed.haydder@gmail.com>
2022-10-18 08:24:59 +0300

    register models to admin app

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import User, UserType, Skill, Job, WorkType, EmploymentType, Category
 # Register your models here.
+
+admin.register(User)
+admin.register(UserType)
+admin.register(Skill)
+admin.register(Job)
+admin.register(WorkType)
+admin.register(EmploymentType)
+admin.register(Category)
