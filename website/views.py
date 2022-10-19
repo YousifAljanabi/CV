c0d2a724ae911c6802e20fd42e81290828768824
Mohammed Hayder <mohammed.haydder@gmail.com>
2022-10-19 11:47:08 +0300

    updated views

--- website/views.py
+++ website/views.py
@@ -1,81 +1,84 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.core import serializers
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.urls import reverse
 
 from django.db import IntegrityError
 
+from .models import User
+
+SIGNUP_PAGE = "website/signup.html"
+
 
 def index(request):
-    return render(request, "website/index.html",)
+    return render(request, "website/index.html", )
 
 
-def login(request):
-    # if request.method == "POST":
+def user_login(request):
+    if request.method == "POST":
 
-    #     # Attempt to sign user in
-    #     username = request.POST["username"]
-    #     password = request.POST["password"]
-    #     user = authenticate(request, username=username, password=password)
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
 
-    #     # Check if authentication successful
-    #     if user is not None:
-    #         login(request, user)
-    #         return HttpResponseRedirect(reverse("index"))
-    #     else:
-    #         return render(request, "network/login.html", {
-    #             "message": "Invalid username and/or password."
-    #         })
-    # else:
-        return render(request, "website/login.html",)
-    
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "website/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "website/login.html", )
 
 
 def signup(request):
-    return render(request, "website/signup-type-select.html",)
+    return render(request, "website/signup-type-select.html", )
 
 
-def createSeeker(request):
+def create_seeker(request):
     if request.method == "GET":
-
-        return render(request, "website/signup.html",{
-         "type" : "seeker"
+        return render(request, SIGNUP_PAGE, {
+            "type": "seeker"
         })
 
-def createEmployer(request):
-    return render(request, "website/signup.html",{
-        "type" : "employer"
+
+def create_employer(request):
+    return render(request, SIGNUP_PAGE, {
+        "type": "employer"
     })
 
 
-def createAccount(request):
-    # if request.method == "POST":
-    #     username = request.POST["username"]
-    #     email = request.POST["email"]
-    #     # name = request.POST["name"]
-    #     # phone = request.POST["phone"]
-    #     # Ensure password matches confirmation
-    #     password = request.POST["password"]
-    #     confirmation = request.POST["confirmation"]
-    #     if password != confirmation:
-    #         return render(request, "website/signup.html", {
-    #             "message": "Passwords must match."
-    #         })
+def create_account(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        # name = request.POST["name"]
+        # phone = request.POST["phone"]
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, SIGNUP_PAGE, {
+                "message": "Passwords must match."
+            })
 
-    #     # Attempt to create new user
-    #     try:
-    #         user = User.objects.create_user(username, email, password)
-    #         user.save()
-    #     except IntegrityError:
-    #         return render(request, "website/signup.html", {
-    #             "message": "Username already taken."
-    #         })
-    #     login(request, user)
-    #     return HttpResponseRedirect(reverse("index"))
-    # else:
-        return render(request, "website/signup.html")
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, SIGNUP_PAGE, {
+                "message": "Username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, SIGNUP_PAGE)
 
 
-
-def jobDetails(request):
+def job_details(request):
     return render(request, "website/job.html")
