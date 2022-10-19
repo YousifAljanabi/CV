04b749c9b98f838b6da7e6d752eabc3e31539d2b
Mohammed Hayder <mohammed.haydder@gmail.com>
2022-10-19 11:46:52 +0300

    turned routes snake case

--- website/urls.py
+++ website/urls.py
@@ -1,14 +1,16 @@
-from django.shortcuts import render
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
+
 from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('login', views.login, name="login"),
+    path('test', csrf_exempt(views.test), name="test"),
+    path('login', views.user_login, name="login"),
     path('signup', views.signup, name="signup"),
-    path('signup/seeker', views.createSeeker, name="createSeeker"),
-    path('signup/employer', views.createEmployer, name="createEmployer"),
-    path('createAccount', views.createAccount, name="createAccount"),
-    path('jobDetails', views.jobDetails, name="jobDetails"),
+    path('signup/seeker', views.create_seeker, name="create_seeker"),
+    path('signup/employer', views.create_employer, name="create_employer"),
+    path('create_account', views.create_account, name="create_account"),
+    path('job_details', views.job_details, name="job_details"),
 
 ]
